1a3d5004bf0175da13c51c7effd6feb73de36faf
Amit Kumar Verma <amitkumar.verma02@ad.infosys.com>
2025-05-28 10:03:05 +0530

    1d

--- Tasks/AdvancedAnalytical/CompletionRateAnalysis/1d.py
+++ Tasks/AdvancedAnalytical/CompletionRateAnalysis/1d.py
@@ -5,16 +5,33 @@ spark = SparkSession.builder.getOrCreate()
 
 # creating dataframe
 engagementDF = spark.read.parquet('./datasets/EngagementData.parquet')
+usersDF = spark.read.parquet('./datasets/UserWatchData.parquet')
 
 # create temp view
 engagementDF.createOrReplaceTempView('engagements')
+usersDF.createOrReplaceTempView('users')
 
 # user with most completed movies
 query = """
-    SELECT userid, completionpercent
-    FROM engagements
-    WHERE completionpercent = 100
+    WITH completedUsers AS (
+        SELECT userid, completionpercent
+        FROM engagements
+        WHERE completionpercent = 100
+    ),
+    countUsers AS (
+        SELECT userid, COUNT(*) AS userCount
+        FROM completedUsers
+        GROUP BY userid
+    ),
+    maxCountUsers AS (
+        SELECT MAX(userCount) AS maxUserCount
+        FROM countUsers
+    )
+    SELECT c.userid, c.userCount
+    FROM countUsers c
+    JOIN maxCountUsers m ON c.userCount = m.maxUserCount
 """
+
 sqlDF = spark.sql(query)
 sqlDF.show()
 
