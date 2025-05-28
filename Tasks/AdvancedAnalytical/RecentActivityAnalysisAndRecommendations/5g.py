8c49b26019d0412b301baf5e08779b5b06816982
Amit Kumar Verma <amitkumar.verma02@ad.infosys.com>
2025-05-28 19:37:41 +0530

    5g

--- Tasks/AdvancedAnalytical/RecentActivityAnalysisAndRecommendations/5g.py
+++ Tasks/AdvancedAnalytical/RecentActivityAnalysisAndRecommendations/5g.py
@@ -5,9 +5,11 @@ spark = SparkSession.builder.getOrCreate()
 
 # creating dataframe
 usersDF = spark.read.parquet('./datasets/UserWatchData.parquet')
+contentDF = spark.read.parquet('./datasets/ContentData.parquet')
 
 # create temp view
 usersDF.createOrReplaceTempView('users')
+contentDF.createOrReplaceTempView('content')
 
 # recent activity
 recentContentDF = spark.sql("SELECT * FROM users ORDER BY timestamp DESC")
@@ -16,14 +18,40 @@ recentContentDF.show()
 # creating tempview for recentData
 recentContentDF.createOrReplaceTempView('recentWatchedContent')
 
-# 
+# content details from recent watchhistory
 query1 = """
-    SELECT c.*
+    SELECT r.userid, c.genre, c.actors, c.director
     FROM recentWatchedContent r
     JOIN content c ON r.showid = c.showid
 """
 
-sq1DF = 
+# 
+query2="""
+    SELECT actor, COUNT(DISTINCT(userid)) AS uniqueUsers
+    FROM (
+        SELECT r.userid, c.showid, EXPLODE(SPLIT(c.actors, '\\\\|')) AS actor
+        FROM content c
+        JOIN recentWatchedContent r ON r.showid c.showid
+    ) t
+    GROUP BY actor
+    ORDER BY uniqueUsers DESC
+"""
+
+query3 = """
+    SELECT c.showid, c.genre
+    FROM recentWatchedContent r
+    JOIN content c ON r.showid = c.showid
+    WHERE r.rating >= 4
+    GROUP BY c.showid, c.genre
+"""
+
+sq1DF = spark.sql(query1)
+sql2DF = spark.sql(query2)
+recommendDF = spark.sql(query3)
+
+sq1DF.show()
+sql2DF.show()
+recommendDF.show()
 
 
 
