c4e874ed93193d4ee1f6d0b6ca2e38d5a6a26123
Amit Kumar Verma <amitkumar.verma02@ad.infosys.com>
2025-05-29 18:59:24 +0530

    F5

--- Tasks/AdvancedAnalytical/F-ContentAnalysis/f5.py
+++ Tasks/AdvancedAnalytical/F-ContentAnalysis/f5.py
@@ -11,20 +11,23 @@ contentDF = spark.read.parquet('./datasets/ContentData.parquet')
 userDF.createOrReplaceTempView('users')
 contentDF.createOrReplaceTempView('content')
 
-# joined table
-query = """
-    WITH userWatchedGenres AS (
-        SELECT u.userid, c.genre, CONCAT_WS(',', COLLECT_LIST(c.showid)) AS shows
-        FROM content c
-        JOIN users u ON u.showid = c.showid
-        GROUP BY c.userid, c.genre
-        ORDER BY LENGTH(shows)
-    )
-"""
 
-sqlDF = spark.sql(query)
-sqlDF.show()
+# movies from fav genre that users have not watched
+query = """
+    WITH genreRank AS (
+        SELECT u.userid, c.genre, u.showid, CAST(u.rating AS INT) AS rating, RANK() OVER (PARTITION BY u.userid ORDER BY u.rating DESC) AS genreRanking
+        FROM users u
+        JOIN content c ON u.showid = c.showid
+    )
+    SELECT ug.userid, ug.genre AS favGenre, c.showid AS suggestedShow
+    FROM content c
+    JOIN genreRank ug ON c.genre = ug.genre AND genreRanking = 1 AND c.showid != ug.showid
+    ORDER BY ug.userid, favGenre, suggestedShow
+"""
+moviesDF = spark.sql(query)
+moviesDF.show()
+
 
 # save the output as parquet
-output_path = "./output/advanced/F-ContentAnalysis/showsWitGenreNotWatched.parquet"
-sqlDF.write.mode('overwrite').parquet(output_path)
+output_path = "./output/advanced/F-ContentAnalysis/showsWithGenreNotWatched.parquet"
+moviesDF.write.mode('overwrite').parquet(output_path)
