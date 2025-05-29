f196d9e10dc697fb33d6808daae9f2e0f649c5d2
Amit Kumar Verma <amitkumar.verma02@ad.infosys.com>
2025-05-29 10:17:58 +0530

    complete

--- Tasks/DataCleaning/sql-core/e.py
+++ Tasks/DataCleaning/sql-core/e.py
@@ -5,6 +5,6 @@ from pyspark.sql.functions import split, col, explode
 spark = SparkSession.builder.getOrCreate()
 
 # create dataframe
-content_df = spark.read.csv('./datasets/EngagementRDD.csv', schema="UserID string,ShowID string,PlaybackStarted string,PlaybackStopped string,CompletionPercent int",inferSchema=True, header=False)
+engagement_df = spark.read.csv('./datasets/EngagementRDD.csv', schema="UserID string,ShowID string,PlaybackStarted string,PlaybackStopped string,CompletionPercent int",inferSchema=True, header=False)
 
-content_df.write.mode("overwrite").parquet('./datasets/EngagementData.parquet')
+engagement_df.write.mode("overwrite").parquet('./datasets/EngagementData.parquet')
